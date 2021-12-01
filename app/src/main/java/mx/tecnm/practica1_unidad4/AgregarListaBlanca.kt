202ae96c44e88ba11b2b0ae1005e21357b3622e8
Alexis Torres Acosta <altorresac@ittepic.edu.mx>
2021-11-30 18:49:25 -0700

    Cambios

--- app/src/main/java/mx/tecnm/practica1_unidad4/AgregarListaBlanca.kt
+++ app/src/main/java/mx/tecnm/practica1_unidad4/AgregarListaBlanca.kt
@@ -3,11 +3,13 @@ package mx.tecnm.practica1_unidad4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import mx.tecnm.practica1_unidad4.ModelosBD.CONTACTO
 import mx.tecnm.practica1_unidad4.databinding.ActivityAgregarListaBlancaBinding
 
 class AgregarListaBlanca : AppCompatActivity() {
     private lateinit var b: ActivityAgregarListaBlancaBinding
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityAgregarListaBlancaBinding.inflate(layoutInflater)
@@ -19,6 +21,9 @@ class AgregarListaBlanca : AppCompatActivity() {
             c.bloqueado = 0
             if(c.insertar()){
                 Toast.makeText(this,"Agregado con exito", Toast.LENGTH_LONG).show()
+                db.collection("Contactos lista blanca").document().set(
+                    hashMapOf("Telefono" to c.numero,"nombre" to c.nombre )
+                )
             }
             finish()
         }
--- app/src/main/java/mx/tecnm/practica1_unidad4/AgregarListaNegra.kt
+++ app/src/main/java/mx/tecnm/practica1_unidad4/AgregarListaNegra.kt
@@ -3,6 +3,7 @@ package mx.tecnm.practica1_unidad4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import mx.tecnm.practica1_unidad4.Modelos.Contacto
 import mx.tecnm.practica1_unidad4.ModelosBD.CONTACTO
 import mx.tecnm.practica1_unidad4.databinding.ActivityAgregarListaNegraBinding
@@ -10,6 +11,7 @@ import mx.tecnm.practica1_unidad4.databinding.ActivityMainBinding
 
 class AgregarListaNegra : AppCompatActivity() {
     private lateinit var b: ActivityAgregarListaNegraBinding
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityAgregarListaNegraBinding.inflate(layoutInflater)
@@ -22,6 +24,9 @@ class AgregarListaNegra : AppCompatActivity() {
             c.bloqueado = 1
             if(c.insertar()){
                 Toast.makeText(this,"Agregado con exito",Toast.LENGTH_LONG).show()
+                db.collection("Contactos lista Negra").document().set(
+                    hashMapOf("Telefono" to c.numero,"nombre" to c.nombre )
+                )
             }
 
             finish()
